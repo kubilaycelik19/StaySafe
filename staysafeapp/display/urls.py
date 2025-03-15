a0ac61d52e41e8b4e6ea4af2322ae265ce11fc35
kubilay Celik <celikkubilay4006@gmail.com>
2025-03-15 17:32:53 +0300

    camera_control&design

--- staysafeapp/display/urls.py
+++ staysafeapp/display/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('index', views.index, name='index'),
     path('video_feed', views.video_feed, name='video_feed'),
+    path('toggle_camera', views.toggle_camera, name='toggle_camera'),
 ]
--- staysafeapp/display/views.py
+++ staysafeapp/display/views.py
@@ -1,4 +1,4 @@
-from django.http import StreamingHttpResponse
+from django.http import StreamingHttpResponse, JsonResponse
 from django.shortcuts import render
 from static.staysafe_main import StaySafe
 
@@ -9,9 +9,19 @@ label_to_name = {
     1: "Kubilay"    # 1 etiketinin ismi
 }
 
+# Kamera durumu için global değişken
+camera_is_active = True
+
 # Create your views here.
 def index(request):
-    return render(request, 'display/index.html')
+    return render(request, 'display/index.html', {'camera_is_active': camera_is_active})
 
 def video_feed(request):
+    if not camera_is_active:
+        return StreamingHttpResponse('')
     return StreamingHttpResponse(stay_safe.SafetyDetector(Source=0, recognition=True), content_type="multipart/x-mixed-replace; boundary=frame")
+
+def toggle_camera(request):
+    global camera_is_active
+    camera_is_active = not camera_is_active
+    return JsonResponse({'status': 'success', 'camera_is_active': camera_is_active})
